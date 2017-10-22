0a14585b133a71499d65bc83718d8a92b2c92cfe
Zachary Hervieux-Moore <zhm@orf-C02TK1KPH3QX.local>
2017-10-21 21:31:48 -0700

    Money2020 hackathon 2017

---
+++ money2020/js/main.js
@@ -0,0 +1,273 @@
+var merchant_id = null;
+var employee_id = null;
+var client_id = null;
+var access_token = null;
+var client_secret = "6c7c21d7-542b-8817-bfc1-7f2661692037";
+var amount = 0;
+var payment_token = "AXE6V6W7X4DRJ";
+
+$( document ).ready(function() {
+    var urlParams = new URLSearchParams(window.location.search + window.location.hash.replace("#", "&"));
+
+    merchant_id = urlParams.get("merchant_id");
+	employee_id = urlParams.get("employee_id");
+	client_id = urlParams.get("client_id");
+	access_token = urlParams.get("access_token");
+
+	make_categories();
+
+	$("footer").click(function() {
+		$('#signup_modal').modal()
+	});
+
+	$("#pay_button").click(function() {
+		process_payment();
+
+		$(this).parent().html('<img class="spinning" src="svg/reload.svg">');
+	});
+});
+
+function make_categories() {
+
+	url = "https://apisandbox.dev.clover.com/v3/merchants/" + merchant_id + "/categories?access_token=" + access_token;
+
+	$.ajax({
+        url: url,
+        type: 'GET',
+        success: function(data) {
+        	var categories = data.elements;
+
+        	for (i = 0; i < categories.length; i++) {
+        		$( "#accordion" ).append(
+        			'<div class="card m-1" id="category-' + categories[i].id + '">\
+        				<a data-toggle="collapse" href="#collapse-' + categories[i].id + '">\
+		        			<div class="card-header" role="tab" class="category">\
+		        				<h5 class="mb-0">'
+							        + categories[i].name +
+		        				'</h5>\
+		        			</div>\
+		        		</a>\
+	        		</div>');
+        		make_items(categories[i].id);
+        	}
+        }
+    });
+
+}
+
+function make_items(category_id) {
+
+	url = "https://apisandbox.dev.clover.com/v3/merchants/" + merchant_id + "/categories/" + category_id + "/items?access_token=" + access_token;
+
+	$.ajax({
+        url: url,
+        type: 'GET',
+        success: function(data) {
+        	var items = data.elements;
+
+    		$( "#category-" + category_id ).append(
+    			'<div id="collapse-' + category_id + '" class="collapse" role="tabpanel" data-parent="#accordion">\
+    				<div class="card-body container">');
+
+    		for (i = 0; i < items.length; i++) {
+    			$( "#category-" + category_id + " .card-body" ).append(
+    					'<div id="item-' + items[i].id + '" class="item row mb-1">\
+    						<div class="col-5"><span>' + items[i].name + '</span></div>\
+    						<div class="col-3">$' + (items[i].price/100).toFixed(2) + '</div>\
+    						<div class="col-4">\
+	    						<div class="input-group">\
+						          <span class="minus input-group-addon">\
+						              <img src="svg/minus.svg">\
+						          </span>\
+						          <input type="number" class="form-control" value="0" min="0" max="10">\
+						          <span class="plus input-group-addon">\
+					                  <img src="svg/plus.svg">\
+						          </span>\
+					          	</div>\
+					      	</div>\
+							<span class="d-none code">' + items[i].code + '</span>\
+    					</div>'
+				);
+			}
+
+			$( "#category-" + category_id ).append(      	
+			      	'</div>\
+			     </div>'
+			);
+
+			$("#category-" + category_id + " .plus").click(function() {
+				var num = parseInt($(this).siblings("input").val());
+
+				if (num < 10) {
+					num = num + 1;
+
+					var cost = $(this).parent().parent().prev().text();
+					cost = cost.substr(1);
+
+					amount = amount + parseFloat(cost);
+
+					$("#total").text(amount.toFixed(2));
+				}
+				else {
+					num = 10;
+				}
+
+				$(this).siblings("input").val(num);
+			});
+
+			$("#category-" + category_id + " .minus").click(function() {
+				num = parseInt($(this).siblings("input").val());
+
+				if (num > 0) {
+					num = num - 1;
+
+					var cost = $(this).parent().parent().prev().text();
+					cost = cost.substr(1);
+
+					amount = amount - parseFloat(cost);
+
+					$("#total").text(amount.toFixed(2));
+				}
+				else {
+					num = 0;
+				}
+
+				$(this).siblings("input").val(num);
+			});
+        }
+    });
+}
+
+function add_line_items(order_id, items) {
+	url = "https://apisandbox.dev.clover.com/v3/merchants/" + merchant_id + "/orders/" + order_id + "/line_items?access_token=" + access_token;
+
+	for (item of items) {
+		$.ajax({
+	        url: url,
+	        type: 'POST',
+	        data: JSON.stringify({ "item": {"id": item.id }}),
+    		dataType: 'json'
+	    });
+	}
+}
+
+function process_payment() {
+	var items = [];
+
+	$(".item").each( function() {
+		var id = $(this).attr("id").substr(5);
+		var quantity = $(this).find("input").val();
+		var name = $(this).first().first().text();
+		var cost = parseFloat($(this).children().eq(1).text().substr(1))*100;
+		var code = $(this).find(".code").text();
+
+		if(quantity > 0) {
+			items.push({id: id, quantity: quantity, name: name, cost: cost, code: code});
+		}
+	});
+
+	url = "https://apisandbox.dev.clover.com/v3/merchants/" + merchant_id + "/orders?access_token=" + access_token;
+
+	if (items.length > 0) {
+		var order;
+		var exponent;
+		var modulus;
+		var prefix;
+		var payload;
+		var encrypted_card;
+
+		$.ajax({
+	        url: url,
+	        type: 'POST',
+	        data: JSON.stringify({"state": "on", "total": amount*100}),
+	        success: function(data) {
+	        	order = data;
+	        	add_line_items(order.id, items);
+	        }
+	    }).done( function() {
+			url = "https://apisandbox.dev.clover.com/v2/merchant/" + merchant_id + "/pay/key?access_token=" + access_token;
+
+	    	payload = {
+				"orderId": order.id, 
+				"taxAmount": 0, 
+				"expMonth": 12, 
+				"cvv": "111", 
+				"amount": amount*100, 
+				"expYear": 2018,
+				"cardNumber": "4761739001010010", 
+			};
+
+	    	$.ajax({
+		        url: url,
+		        type: 'GET',
+		        data: JSON.stringify(payload),
+		        success: function(data) {
+					modulus = data.modulus;
+					exponent = data.exponent;
+					prefix = data.prefix;
+		        }
+		    }).done( function() {
+		    	url = "https://bluwave.herokuapp.com/api/orders/generate_card_encryption?prefix=" + prefix + "&card=" + payload.cardNumber + "&exponent=" + exponent + "&modulus=" + modulus
+		    
+		    	$.ajax({
+			        url: url,
+			        type: 'GET',
+			        success: function(data) {
+						encrypted_card = data.encryption
+			        }
+			    }).done( function() {
+	    			url = "https://apisandbox.dev.clover.com/v2/merchant/" + merchant_id + "/pay?access_token=" + access_token;
+
+	    			payload = {
+					    "orderId": order.id,
+					    "currency": "usd",
+					    "taxAmount": 0, 
+						"expMonth": 12, 
+						"cvv": "111", 
+						"amount": amount*100, 
+						"expYear": 2018,
+					    "cardEncrypted": encrypted_card,
+					    "last4": "0010",
+					    "first6": "476173"
+					}
+
+	    			$.ajax({
+				        url: url,
+				        type: 'POST',
+				        data: JSON.stringify(payload),
+				        success: function(data) {
+							if (data.result == "APPROVED") {
+
+								url = "https://bluwave.herokuapp.com/api/orders.json";
+
+								codes = [];
+
+								for (item of items) {
+									codes.push(item.code);
+								}
+
+								console.log(codes);
+
+								payload = {
+									"person_id": 1,
+									"item_ids": codes
+								};
+
+								$.ajax({
+							        url: url,
+							        type: 'POST',
+							        data: payload,
+							        success: function(data) {
+										$('#signup_modal').modal('hide');
+							        }
+							    });
+							}
+				        }
+				    });
+			    });
+		    });
+	    });
+	}
+}
+
+
